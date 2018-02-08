36219f3ee40d587f87a7f3af9fb591963667b606
thiagovb46 <32931894+thiagovb46@users.noreply.github.com>
2018-02-08 11:13:52 -0200

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 echo "Teste de conflito no git "
 //echo "Testando o Git para controle de versão";
 echo "Foi feita alteração";
-echo  "TEsting the git"
+echo  "TEsting the git" 
+  $teste =2 ;
+  $teste+=1;
+echo $teste;
 
 ?>
